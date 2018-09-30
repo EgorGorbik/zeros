18369ec9bc3e52675ddeb1fd1d7e10d206631c6c
Egor Gorbik <gorbikega@gmail.com>
2018-09-30 21:38:19 +0300

    add zeros task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+var result = 0;
+
+for (var i = 5 ; i <= number ; i*=5) {
+	var temp = Math.floor(number/i);
+	result += temp;
+} 
+	return result;
 }
